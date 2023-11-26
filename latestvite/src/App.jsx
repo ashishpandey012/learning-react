dff31592420a8ae224d180ebf34140d99c2d18f1
ashishpandey012 <115835529+ashishpandey012@users.noreply.github.com>
2023-11-26 16:17:19 +0530

    new project added

---
+++ latestvite/src/App.jsx
@@ -0,0 +1,10 @@
+import New from './new';
+function App() {
+  return (
+    <>
+       <h1>React with vite | Ashish Pandey</h1>
+       <New />
+    </>
+  )
+}
+export default App;
---
+++ latestvite/src/new.jsx
@@ -0,0 +1,6 @@
+function New(){
+    return(
+        <h2>Hey! This is my new component</h2>
+    )
+}
+export default New;
